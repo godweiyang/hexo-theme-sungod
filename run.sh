aac9f05ff7fec3f6efa42d8f11a43ecc74152cb8
godweiyang <792321264@qq.com>
2020-03-11 19:01:17 +0800

    update some features

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
+hexo clean
 hexo g
 hexo d
-git add .
+git add -A
 git commit -m "$1"
 git push origin master
---
+++ themes/matery/source/libs/codeBlock/codeCopy.js
@@ -0,0 +1,12 @@
+// 代码块一键复制
+
+$(function () {
+    var $copyIcon = $('<i class="fa fa-copy code_copy" title="复制代码" aria-hidden="true"></i>');
+    
+    $('.code-area').prepend($copyIcon);
+    new ClipboardJS('.fa-copy', {
+        target: function (trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+});
